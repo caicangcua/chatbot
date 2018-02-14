93bd776668eea61dd2c9210af9b3403d193a756a
caicangcua <36462470+caicangcua@users.noreply.github.com>
2018-02-14 17:27:59 +0700

    y
    y

--- index.php
+++ index.php
@@ -42,7 +42,9 @@ if(preg_match('[time|current time|now|время|час]', strtolower($message))
         $message_to_reply = "Sorry, I don't know.";
     }
 } else {
-    $message_to_reply = 'Sorry, I don\'t understand you. because i'm chatbot.';
+
+    $message_to_reply = 'Sorry, I don\'t understand you. I can only tell what time it is now.';
+
 }
 
 //API Url
